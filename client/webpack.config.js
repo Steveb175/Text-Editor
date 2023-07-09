ac9d75fa30edf087471df978203071eb424d1f15
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-07-09 13:56:18 -0500

    Fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+//Webpack configuration
 module.exports = () => {
   return {
     mode: "development",
@@ -71,8 +69,8 @@ module.exports = () => {
             options: {
               presets: ["@babel/preset-env"],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
                 "@babel/transform-runtime",
+                "@babel/plugin-proposal-object-rest-spread",
               ],
             },
           },
