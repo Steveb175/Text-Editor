c4ff2330349cea98d03effd1546af20daf17ac8f
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-07-09 11:54:52 -0500

    editor.js updated

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,42 +1,59 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
-import { header } from './header';
+import { getDb, putDb } from "./database";
+import { header } from "./header";
 
 export default class {
   constructor() {
-    const localData = localStorage.getItem('content');
+    const localData = localStorage.getItem("content");
 
     // check if CodeMirror is loaded
-    if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
+    if (!window.CodeMirror) {
+      throw new Error("CodeMirror is not loaded or initiated properly");
     }
 
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
-      lineNumbers: true,
-      lineWrapping: true,
-      autofocus: true,
-      indentUnit: 2,
-      tabSize: 2,
-    });
+    try {
+      this.editor = CodeMirror(document.querySelector("#main"), {
+        value: "",
+        mode: "javascript",
+        theme: "monokai",
+        lineNumbers: true,
+        lineWrapping: true,
+        autofocus: true,
+        indentUnit: 2,
+        tabSize: 2,
+      });
+    } catch (error) {
+      console.error("Failed to initialize CodeMirror", error);
+      return;
+    }
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.info("Loaded data from IndexedDB, injecting into editor");
+      const value = data || localData || header;
+
+      // Check if the value is a string and if it's not empty
+      if (typeof value !== "string" || !value.trim()) {
+        console.error("Unexpected or empty value", value);
+        return;
+      }
+
+      // Set the value of the editor
+      this.editor.setValue(value);
     });
 
-    this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
+    this.editor.on("change", () => {
+      localStorage.setItem("content", this.editor.getValue());
     });
 
     // Save the content of the editor when the editor itself is loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+    this.editor.on("blur", () => {
+      console.log("The editor has lost focus");
+      putDb(localStorage.getItem("content"));
     });
+  }
+  toString() {
+    return this.editor.getValue();
   }
 }
